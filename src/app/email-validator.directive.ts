29cdeb3f27ee523e8ec5e9f65532e6941708dc8c
David Tobar <Mac_Sit@iMac-de-Mac-sit.local>
2016-12-27 11:24:46 -0500

    directiva email

--- src/app/email-validator.directive.ts
+++ src/app/email-validator.directive.ts
@@ -1,24 +1,31 @@
-import { ElementRef, Renderer, Directive } from '@angular/core';
+import { ElementRef, Renderer, Directive, Output, HostBinding } from '@angular/core';
 
 @Directive({
   selector: 'input[validateEmail]',
-  host: {'(blur)': 'onBlur($event)'}
+  host: { '(blur)': 'onBlur($event)' },
+
 })
 export class EmailValidatorDirective {
-  constructor(private el:ElementRef, private renderer:Renderer) {}
+  constructor(private el: ElementRef, private renderer: Renderer) { }
+
+  @HostBinding('class.invalido') correcto: boolean = false;
 
   //Funcion que evalua el patron de email ingresado y retorna el foco si hay errores	
   //@return: void
   onBlur($event) {
 
-  	let value = this.el.nativeElement.value;
+    let value = this.el.nativeElement.value;
     let pattern = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-    if((pattern.test(value))||(value == "")){
-    	//this.el.nativeElement.style.border = '3px solid #3ad216'; //Valido(true)
-    }else {
-    	//this.el.nativeElement.style.border = '3px solid red'; //Invalido(false)
-    	this.el.nativeElement.focus();
+    if ((pattern.test(value)) || (value == "")) {
+     
+      this.correcto = false;
+    } else {
+     
+      //this.el.nativeElement.focus();
+      this.correcto = true;
     }
 
+
   }
+
 }
